302e18cb35f874a3b75a7bdb81b93d8ad4fbd2f5
peo5 <pedromsimoesferreira@gmail.com>
2022-01-20 22:52:26 -0300

    Tilemap and better movement

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,18 +1,20 @@
 extends KinematicBody2D
 
-const move_speed = 200
-const jump_force = -80
-const gravity = 40
+const sscale = 20
+const move_speed = 250 * sscale
+const jump_force = -8 * sscale
+const gravity = 10 * sscale
 
 var vel = Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
 	var move_dir = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
-	vel.x = move_speed * move_dir
-
-	if(Input.is_action_just_pressed("jump")):
-		vel.y += jump_force
+	vel.x = move_speed * move_dir * delta
 	
-	vel.y += gravity*delta
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			vel.y = jump_force
+	else:
+		vel.y += gravity*delta
 	
-	move_and_slide(vel)
+	move_and_slide(vel, Vector2.UP)
